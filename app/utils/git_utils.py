121d6cee15f0243ef9aeb712dd10b4b7d346d1b5
ryosuke.mondo <ryosuke.mondo@gmail.com>
2024-10-26 22:37:18 +0900

    Refactor git_utils.py to improve file tracking performance

--- app/utils/git_utils.py
+++ app/utils/git_utils.py
@@ -1,17 +1,26 @@
 import subprocess
 import logging
+import tempfile
+import os
 
 
 async def get_tracked_files(destination):
     try:
-        result = subprocess.run(
-            ['git', 'ls-files', '--cached', '--others', '--exclude-standard'],
-            cwd=destination,
-            capture_output=True,
-            text=True,
-            encoding='utf-8'
-        )
-        tracked_files = result.stdout.splitlines()
+        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+            tmp_file_path = tmp_file.name
+            subprocess.run(
+                ['git', 'ls-files', '--cached', '--others', '--exclude-standard'],
+                cwd=destination,
+                stdout=tmp_file,
+                text=True,
+                encoding='utf-8'
+            )
+
+        with open(tmp_file_path, 'r', encoding='utf-8') as file:
+            tracked_files = file.read().splitlines()
+
+        os.remove(tmp_file_path)
+
         logging.debug(f'Tracked files: {tracked_files}')
         return tracked_files
     except Exception as e:
